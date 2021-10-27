ba55c7a3286697c302032bf282878b427fc31348
Aal Emraan <developer.aalemraan@gmail.com>
2021-10-27 13:25:27 +0600

    new api added

--- index.js
+++ index.js
@@ -66,6 +66,9 @@ run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('hello there.')
 })
+app.get('/hello', (req, res) => {
+    res.send('this is hello.')
+})
 
 app.listen(port, () => {
     console.log('listening from port: ', port)
